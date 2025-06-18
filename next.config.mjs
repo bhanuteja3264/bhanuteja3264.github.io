ae90f341e3adb267ff6a8f08f86a874d17819d98
bhanuteja64 <bhanutejaybt.2004@gmail.com>
2025-06-18 17:38:24 +0530

    ben tennyson

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -11,10 +12,10 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
-}
+  assetPrefix: isProd ? '/Portfolio/' : '',
+  basePath: isProd ? '/Portfolio' : '',
+};
 
-export default nextConfig
+export default nextConfig;
